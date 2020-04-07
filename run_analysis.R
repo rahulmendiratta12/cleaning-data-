7351e271e21fc9c87298b068dd14f44e19291e76
rahulmendiratta12 <33995424+rahulmendiratta12@users.noreply.github.com>
2020-04-07 13:38:44 +0530

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,36 +1,34 @@
-library(dplyr)
+library(plyr)
 
 
-X_train12 <- read.table("./UCI HAR Dataset/train/X_train.txt")
-Y_train23 <- read.table("./UCI HAR Dataset/train/Y_train.txt")
-Sub_train23 <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-
-# read test data
-X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
-Y_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
-Sub_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-
-variable_names <- read.table("./UCI HAR Dataset/features.txt")
-
-activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
-
-X_totalwe <- rbind(X_train, X_test)
-Y_totaler <- rbind(Y_train, Y_test)
-Sub_total <- rbind(Sub_train, Sub_test)
 
 
-selected_var245 <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,2]),]
-X_total123 <- X_total[,selected_var[,1]]
+
+est <- read.table("./UCI HAR Dataset/test/X_test.txt")
+test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
+b_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+
+names <- read.table("./UCI HAR Dataset/features.txt")
+
+abels <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
+we <- cbind(X_train, X_test
+er <- cbind(Y_train, Y_test)
+al <- cbind(Sub_train, Sub_test)
 
 
-colnames(Y_total) <- "activity"
-Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,2]))
-activitylabel <- Y_total[,-1]
+var24555 <- variable_names[grep("mean\\(\\)|std\\(\\)",variable_names[,1]),]
+  al123 <- X_total[,selected_var[,-1]
+
+
+colnames(Y_total) <- "corepo124"
+Y_total$activitylabel <- factor(Y_total$activity, labels = as.character(activity_labels[,1))
+activitylabel <- Y_total[,2]
 
 
 colnameswe(X_total) <- variable_names[selected_var[,1],2]
 
-colnames(Sub_total) <- "subject"
+colnames(Sub_total) <- "important "
 total <- cbind(X_total, activitylabel, Sub_total)
 total_mean <- total %>% group_by(activitylabel, subject) %>% summarize_each(funs(mean))
-write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = FALSE, col.names = TRUE)
+write.table(total_mean, file = "./UCI HAR Dataset/tidydata.txt", row.names = TRUE, col.names = FALSE)
